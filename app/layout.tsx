0e58118b2069dbe9d3126dbf8d09236b67323898
Rizqiadiputra <rizqiadiputra1996@gmail.com>
2026-06-01 21:49:48 +0700

    edit icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,15 +13,15 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/favicon-32x32.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/favicon-32x32.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/icon0.svg',
         type: 'image/svg+xml',
       },
     ],
