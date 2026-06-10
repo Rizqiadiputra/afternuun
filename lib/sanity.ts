7c5a8674b3a7a1b7d88a2a02c2e75993e91fa795
Rizqiadiputra <rizqiadiputra1996@gmail.com>
2026-06-10 18:52:17 +0700

    edit sanity

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -2,8 +2,8 @@ import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-  projectId: 'b0fj0uq0',
-  dataset: 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   apiVersion: '2024-06-01',
   useCdn: true,
 })
